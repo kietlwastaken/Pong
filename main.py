90d6a3060f80f25a040ad021890f779b3daa6ebb
kietlwastaken <131593373+kietlwastaken@users.noreply.github.com>
2025-05-07 10:17:36 +0100

    bug fixes

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ dt = 0
 player_pos = pygame.Vector2((screen.get_width() / 2) - 500, screen.get_height() / 2)
 player2_pos = pygame.Vector2((screen.get_width() / 2) + 500, screen.get_height() / 2)
 
-ball_pos = pygame.Vector2((screen_width() / 2) + 500, screen.get_height() / 2)
-ball_dir = pygame.Vector2(1,0)
+ball_pos = pygame.Vector2((screen.get_width / 2), screen.get_height() / 2)
+ball_vel = pygame.Vector2(1,0)
 ball_radius = 20
 
 player_height = 150
 player_width = 20
+
+player_speed = 400
 
 while running:
     # poll for events
@@ -39,13 +41,13 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        player_pos.y -= player_speed * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        player_pos.y += player_speed * dt
     if keys[pygame.K_UP]:
-        player2_pos.y -= 300 * dt
+        player2_pos.y -= player_speed * dt
     if keys[pygame.K_DOWN]:
-        player2_pos.y += 300 * dt
+        player2_pos.y += player_speed * dt
 
     if player_pos.y >= screen_height - player_height:
         player_pos.y = screen_height - player_height
@@ -54,14 +56,15 @@ while running:
     
     if player2_pos.y >= screen_height - player_height:
         player2_pos.y = screen_height - player_height
-    if player2_pos.y <= screen_height:
-        player2_pos.y = screen_height
+    if player2_pos.y <= 0:
+        player2_pos.y = 0
+
     
     ball_pos += ball_dir * dt
 
-    if ball_pos.x - ball_radius <= 0 or ball_pos.x + ball_radius >= 800:
+    if ball_pos.x - ball_radius <= 0 or ball_pos.x + ball_radius >= screen_width:
         ball_vel.x *= -1
-    if ball_pos.y - ball_radius <= 0 or ball_pos.y + ball_radius >= 600:
+    if ball_pos.y - ball_radius <= 0 or ball_pos.y + ball_radius >= screen_height:
         ball_vel.y *= -1
 
     ball_rect = pygame.Rect(ball_pos.x - ball_radius, ball_pos.y - ball_radius, ball_radius * 2, ball_radius * 2)

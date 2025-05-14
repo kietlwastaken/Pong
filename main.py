1d5c25b05b247714532fae42a40df94b4e9a18ad
kietlwastaken <131593373+kietlwastaken@users.noreply.github.com>
2025-05-14 08:40:32 +0100

    a

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ m = tk.Tk()
 
 m.title("Pong!")
 
+def run():
+    game.pygamerun(playerSpeed.get(), ballSpeed.get())
 
 ballSpeed = tk.Scale(m, from_=100, to=1000, orient=HORIZONTAL)
 playerSpeed = tk.Scale(m, from_=400, to=1000, orient=HORIZONTAL)
 
-playBtn = tk.Button(m, width=uiwidth, text="run", command=game.pygamerun)
+playBtn = tk.Button(m, width=uiwidth, text="run", command=run)
